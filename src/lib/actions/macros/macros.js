63d63024e8c196180220773a6def0c4920524016
A-Pradeep <sharmapradeep872@gmail.com>
2018-08-14 13:54:41 +0530

    Update

--- src/lib/actions/macros/macros.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const fs = require('fs');
-let macros = fs.readFileSync(`${__dirname}/table.html`).toString();
-macros += fs.readFileSync(`${__dirname}/value.html`).toString();
-module.exports = macros;
--- src/lib/actions/properties/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = function(router) {
-  return {
-    reference: require('./reference')(router)
-  };
-};
